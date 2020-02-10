35b9528bb5b49ee2f8572ebfcb9963030cb108a8
xiekch <xiekch@qq.com>
2020-02-10 16:08:23 +0800

    transaction propagation

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.demo.dao.UserDaoImplTemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service("userService")
@@ -12,10 +13,23 @@ public class UserService {
     @Autowired
     private UserDaoImplTemplate userDao;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void transfer(int from, int to, int amount) {
         userDao.updateBalance(to, amount);
         userDao.updateBalance(from, -amount);
         System.out.println("success");
     }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 2)
+    public void transferException(int from, int to, int amount) {
+        userDao.updateBalance(to, amount);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        userDao.updateBalance(from, -amount);
+        System.out.println("success");
+    }
+
 }
--- demo/src/test/java/com/example/demo/jdbc/TransactionTest.java
+++ demo/src/test/java/com/example/demo/jdbc/TransactionTest.java
@@ -8,6 +8,7 @@ import com.example.demo.util.JDBCUtils;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
 
 public class TransactionTest {
 
@@ -40,8 +41,23 @@ public class TransactionTest {
 
     @Test
     public void testUpdateBalance() {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext ("transaction.xml");
-        UserService userService= (UserService) applicationContext.getBean("userService");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("transaction.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
         userService.transfer(2, 3, 2000);
+    }
+
+    /**
+     * 在用REQUIRES_NEW的时候，发现没有起作用。分析了一下，原因是
+     * A方法（有事务）调用B方法（要独立新事务），如果两个方法写在同一个类里，
+     * spring的事务会只处理能同一个。
+     */
+    @Test
+    @Transactional
+    public void testTransactionPropagation() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("transaction.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.transfer(2, 1, 1000);
+        userService.transferException(4, 3, 1000);
+        System.out.println("finished");
     }
 }
